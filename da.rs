df2a3e11ed3d5374e9dae80cd29150c253154c87
unknown <daniilzolotarev20@gmail.com>
2025-10-31 11:59:57 +0300

    b

---
+++ da.rs
@@ -0,0 +1,8 @@
+fn main(){
+    let  mut s:i32=0;
+    for i in 0..3 {
+        let mut n_input: string = string:: new();
+        std::io::stdin().read_line(&n_input).expect("Error input!!")
+        let n: i32 = n_input.trim().parse
+    }
+}
